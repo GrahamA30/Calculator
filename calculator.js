479686411b448e263b987926d940e1400a390488
GrahamA30 <aj.graham17@gmail.com>
2020-04-22 13:01:39 -0400

    implemented operate() function

--- calculator.js
+++ calculator.js
@@ -11,3 +11,19 @@ function multiply(a,b){
 function divide(a,b){
     return parseFloat(a) / parseFloat(b);
 }
+
+function operate(operator,num1,num2){
+
+    switch(operator){
+        case '+':
+            return add(num1,num2);
+        case '-':
+            return subtract(num1,num2);
+        case '*': 
+            return multiply(num1,num2);
+        case '/':
+            return divide(num1,num2);
+        default:
+            break;
+    }
+}
